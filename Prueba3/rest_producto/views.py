85a4d0f9a87c1a712ae6f8c4cc6038cbb372440a
Morkax <rexo.navarrete.v@gmail.com>
2022-06-06 22:23:19 -0400

    Avance de la prueba

---
+++ Prueba3/rest_producto/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from core.models import Producto
+from .serializers import ProductoSerializer
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def lista_productos(request):
+    if request.method == 'POST':
+        producto = Producto.objects.all()
+        serializer = ProductoSerializer(producto, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProductoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else: 
+            return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
